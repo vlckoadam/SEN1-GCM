80e4454fa758e1c7ac5bb1fc86c759e74e86935a
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2022-09-14 13:29:47 +0200

    Create 1492022_zadanie_3.py

---
+++ 1492022_zadanie_3.py
@@ -0,0 +1,7 @@
+a = int(input("Zadajte číslo 1: "))
+b = int(input("Zadajte číslo 2: "))
+c = int(input("Zadajte číslo 3: "))
+
+list = [a, b, c]
+print("Zadané čísla: ", a, b, c)
+print("Najmenšie číslo", min(list))
