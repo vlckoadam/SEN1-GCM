2f6901afd4516855e1f12bcac0ab9b6babc29937
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2022-09-21 18:18:56 +0200

    Update 2122022_hra_prj.py

--- 2122022_hra_prj.py
+++ 2122022_hra_prj.py
@@ -1,4 +1,5 @@
 import random
+from subprocess import call
 
 f = open('hihi.txt', 'r')
 hl = f.read()
@@ -13,13 +14,14 @@ n = input("Zadaj svoje meno: ")
 list = ["Choď sa osprchovať", "Už si sa sprchoval?", "To čo máte chlapci?", "A to ako vám napadlo?", "Chlapci už zhasnínajte",
         "Ideš na omšu?", "JARO HARAAG!!", f'Zavolám vychošovi, {n}',"Povysávať a potom aj umyť podlahu!", f"Zavolám otcovi, {n}",
         "Bol si v sprche", "Neviem ti odpovedať", "Ideš na šport?"]
-
+call("clear")
 print("Ahoj, ", n)
 t = input()
 if len(t)==0:
     exit()
 
 while len(t)>=1:
+    call("clear")
     n = input()
     if n == "Nie" in "Choď sa osprchovať":
         print("Máš čas do 21:3O!!!")
@@ -28,6 +30,16 @@ while len(t)>=1:
     print(random.choice(list))
     if len(n)==0:
         break
+
+
+
+
+
+
+
+
+
+
 
 
 
