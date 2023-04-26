3af8cd39afbdaabfb04914e49ae19d374bfae754
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-04-26 13:24:32 +0200

    hsdfa

---
+++ potkan-simulator.py
@@ -0,0 +1,18 @@
+import random
+import time
+import os
+
+os.system("clear")
+def eng():
+    print("####################################################")
+    print("#            🐀 POTKAN LIFE SIMULATOR 🐀          #")
+    print("#                                                  #")
+    print("#                                                  #")
+    print("#              🐀 PRESS [A] TO PLAY 🐀            #")
+    print("#              🐀 PRESS [X] TO EXIT 🐀            #")
+    print("#        🐀 PRESS [L] TO CHANGE LANGUAGE🐀        #")
+    print("#                                                  #")
+    print("#                                                  #")
+    print("####################################################")
+
+def 
